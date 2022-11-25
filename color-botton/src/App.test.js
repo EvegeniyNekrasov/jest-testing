b5ba112b8cf164fe524fe4b5ac86a64fb9b1e63d
ECOMT\ealeksandrovich <ealeksandrovich@ecomt.net>
2022-11-25 21:04:11 +0100

    adding the second test and the functionality to the button

--- color-botton/src/App.test.js
+++ color-botton/src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color", () => {
+test("button has correct initial color, and updates when clicked", () => {
   render(<App />);
 
   // Find an element with a role of button and text of "Change to blue"
@@ -11,4 +11,20 @@ test("button has correct initial color", () => {
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 });
 
-test("button turns blue when click", () => {});
+test("button turns blue when click", () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expecting the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be "Change to red"
+  expect(colorButton.textContent).toBe("Change to red");
+});
