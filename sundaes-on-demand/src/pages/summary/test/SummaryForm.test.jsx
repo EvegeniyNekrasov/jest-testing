17b662be1d524044073499106087c90b733abfde
ECOMT\ealeksandrovich <ealeksandrovich@ecomt.net>
2022-11-27 23:37:19 +0100

    making first tests

---
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from "@testing-library/reat";
+import SummaryForm from "../SummaryForm";
+
+test("primera comprobacion", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /term and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("segunda comprobacion", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confimButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+
+  fireEvent.click(checkbox);
+  expect(confimButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confimButton).toBeDisabled();
+});
