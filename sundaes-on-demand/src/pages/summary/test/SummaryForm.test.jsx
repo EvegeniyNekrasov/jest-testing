7cd3551e296bf806ece43092e631fd5d0f711901
ECOMT\ealeksandrovich <ealeksandrovich@ecomt.net>
2022-11-28 08:42:57 +0100

    Adding thirt test and implement SummaryForm with functionality

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("primera comprobacion", () => {
   render(<SummaryForm />);
@@ -14,7 +15,8 @@ test("primera comprobacion", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("segunda comprobacion", () => {
+test("segunda comprobacion", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
 
   const checkbox = screen.getByRole("checkbox", {
@@ -24,9 +26,54 @@ test("segunda comprobacion", () => {
     name: /confirm order/i,
   });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confimButton).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(confimButton).toBeDisabled();
+});
+
+test("tercera comprobacion", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // popover starts out hiden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  const nullPopoverAgain = screen.queryByText(popover)
+  expect(nullPopoverAgain).not.toBeInTheDocument(/no ice cream will actually be delivered/i);
+
+
+  // Querry Methods
+  // command
+  // get: expect element to be in the DOM
+  // query: expect element not to be in the DOM
+  // find: expect element no appear async
+
+  /*
+    (exclude) expect only one match
+    (include) expect more than one match
+  */
+
+  /*
+    QueryType
+    Role(most prefer)
+    AltText(images)
+    Text(display elements)
+    Form elements
+        - PlaceholderText
+        - LabelText
+        - DisplayValue
+    */
 });
